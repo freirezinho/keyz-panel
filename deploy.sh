fdb3cc0414479e809edf37b385692ecbc0d1dd66
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-09-08 14:49:50 -0300

    fix(ci): base url for prod

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cp ./.env.development ./.env.production &&
-sed -i'.bak' -e 's/VITE_REACT_APP_BASE_URL=/VITE_REACT_APP_BASE_URL=\/keyz-panel/g' ./.env.production &&
+sed -i'.bak' -e 's/VITE_REACT_APP_BASE_URL=/VITE_REACT_APP_BASE_URL=\keyz-panel/g' ./.env.production &&
 rm ./.env.production.bak
